6798d915e9d27cadb2d02acf754ec6391fde4d2d
Alexander Boest-Petersen <aboestpetersen@gmail.com>
2021-03-14 19:12:00 +0100

    Delete main_map.js

--- main_map.js
+++
@@ -1,9 +0,0 @@
-var mymap = L.map('map')
-             .setView([50.938056, 6.956944], 13)
-             .addLayer(L.tileLayer(
-               'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                  maxZoom: 18,
-                  attribution: 'Map data © \
-                  <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-                })
-            );
