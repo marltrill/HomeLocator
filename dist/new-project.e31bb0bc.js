89070bd497c343c6cab44d054be77ae656c14388
Alexander Boest-Petersen <aboestpetersen@gmail.com>
2021-03-23 10:30:16 +0100

    test push

--- dist/new-project.e31bb0bc.js
+++ dist/new-project.e31bb0bc.js
@@ -103974,7 +103974,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55498" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60204" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
