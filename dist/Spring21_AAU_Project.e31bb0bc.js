72dc765362b4f03d84a1a2848174943766876db3
Alexander Boest-Petersen <aboestpetersen@gmail.com>
2021-06-03 19:38:42 +0200

    minor fixes

--- dist/Spring21_AAU_Project.e31bb0bc.js
+++ dist/Spring21_AAU_Project.e31bb0bc.js
@@ -115458,7 +115458,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63764" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65099" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
